e4643dbb29c25bbf37f89c0041f8b15d6c7dcaf5
Christopher SHEN <shenleidi@gmail.com>
2024-09-12 16:15:12 +0800

    common.sh: reuse distfiles by nullfs to accelerate building processes

--- build/common.sh
+++ build/common.sh
@@ -639,6 +639,10 @@ setup_distfiles()
 
 	echo ">>> Setting up distfiles in ${DSTDIR}"
 
+	mkdir -p /usr/ports/distfiles
+	mkdir -p ${STAGEDIR}/usr/ports/distfiles
+	mount -t nullfs /usr/ports/distfiles ${STAGEDIR}/usr/ports/distfiles
+
 	DISTFILESET=$(find_set distfiles)
 	if [ -n "${DISTFILESET}" ]; then
 		mkdir -p ${DSTDIR}${PORTSDIR}
@@ -1205,6 +1209,7 @@ setup_stage()
 /boot/msdos
 /dev
 /mnt
+/usr/ports/distfiles
 ${SRCDIR}
 ${PORTSDIR}
 ${COREDIR}
